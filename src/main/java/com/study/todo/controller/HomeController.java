f15d09a2bdfda7fd2f5cab1ca946aa03e131c621
SEOHEE <sh265601@gmail.com>
2019-12-01 22:11:13 +0900

    First commit

---
+++ src/main/java/com/study/todo/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.study.todo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+    @GetMapping
+    public String home() {
+        return "안녕안뇽~~";
+    }
+}
