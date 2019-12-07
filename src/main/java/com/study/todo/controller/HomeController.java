8d69f422b55a2aba1f35ab5c938e848a41d41483
SEOHEE <sh265601@gmail.com>
2019-12-07 19:12:53 +0900

    테스트

--- src/main/java/com/study/todo/controller/HomeController.java
+++ src/main/java/com/study/todo/controller/HomeController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping
     public String home() {
-        return "이야압";
+        return "호이횡";
     }
 }
